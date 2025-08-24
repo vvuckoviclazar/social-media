67cb98f34541258d0e0399e54df73c9fd3805f91
Lazar Vuckovic <lazarvuckovic777@gmail.com>
2025-08-24 08:25:28 +0200

    Maping...

--- social-media/src/App.jsx
+++ social-media/src/App.jsx
@@ -1,10 +1,9 @@
 import { useState } from "react";
 import "./index.css";
-import { friendsData, fixedPosts, userAccount } from "./data";
+import { userAccount } from "./data";
 
 function App() {
-  const [posts, setPosts] = useState(() => structuredClone(fixedPosts));
-  const [friends, setFriends] = useState(friendsData);
+  const [posts, setPosts] = useState(userAccount.posts);
 
   return (
     <>
@@ -63,9 +62,9 @@ function App() {
         <div className="friends-div">
           <div className="width-div">
             <h1 className="friends-h1">Friends</h1>
-            <p>{friends.length} friends</p>
+            <p>{userAccount.friends.length} friends</p>
             <ul className="friends-list">
-              {friends.map((friend, index) => (
+              {userAccount.friends.map((friend, index) => (
                 <li key={index} className="friend-card">
                   <img
                     src={`/${friend.picture}`}
@@ -80,7 +79,7 @@ function App() {
         </div>
 
         <ul className="post-list">
-          {Object.values(posts).map((post, index) => (
+          {posts.map((post, index) => (
             <li key={index} className="post-item">
               <div className="userName-div">
                 <div className="left-mini-div">
@@ -89,7 +88,10 @@ function App() {
                     src={`/${userAccount.picture}`}
                     alt=""
                   />
-                  <h1 className="post-h1">{userAccount.owner}</h1>
+                  <div className="date-name-div">
+                    <h1 className="post-h1">{userAccount.owner}</h1>
+                    <p className="date-p">{post.date}</p>
+                  </div>
                 </div>
 
                 <div className="open-div">
@@ -116,22 +118,45 @@ function App() {
                   </div>
                 </div>
               </div>
+
               <h2 className="post-text">{post.text}</h2>
+
               <div className="like-comment-div">
                 <p className="liked-by-text">
-                  Liked by
-                  {Object.values(post.likes)
-                    .map((l) => l.name)
-                    .join(", ")}
+                  {post.likes.map((l) => l.name).join(", ")} liked this post.
                 </p>
-                <p className="comment-count">
-                  {Object.values(post.comments).length} comments
-                </p>
+                <p className="comment-count">{post.comments.length} comments</p>
               </div>
 
               <div className="post-buttons-div">
-                <button className="like like-btn">Like</button>
-                <button className="comment comment-btn">Comment</button>
+                <button className="like like-btn">
+                  Like
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                    class="size-6"
+                  >
+                    <path d="M7.493 18.5c-.425 0-.82-.236-.975-.632A7.48 7.48 0 0 1 6 15.125c0-1.75.599-3.358 1.602-4.634.151-.192.373-.309.6-.397.473-.183.89-.514 1.212-.924a9.042 9.042 0 0 1 2.861-2.4c.723-.384 1.35-.956 1.653-1.715a4.498 4.498 0 0 0 .322-1.672V2.75A.75.75 0 0 1 15 2a2.25 2.25 0 0 1 2.25 2.25c0 1.152-.26 2.243-.723 3.218-.266.558.107 1.282.725 1.282h3.126c1.026 0 1.945.694 2.054 1.715.045.422.068.85.068 1.285a11.95 11.95 0 0 1-2.649 7.521c-.388.482-.987.729-1.605.729H14.23c-.483 0-.964-.078-1.423-.23l-3.114-1.04a4.501 4.501 0 0 0-1.423-.23h-.777ZM2.331 10.727a11.969 11.969 0 0 0-.831 4.398 12 12 0 0 0 .52 3.507C2.28 19.482 3.105 20 3.994 20H4.9c.445 0 .72-.498.523-.898a8.963 8.963 0 0 1-.924-3.977c0-1.708.476-3.305 1.302-4.666.245-.403-.028-.959-.5-.959H4.25c-.832 0-1.612.453-1.918 1.227Z" />
+                  </svg>
+                </button>
+                <button className="comment comment-btn">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    class="size-6"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M7.5 8.25h9m-9 3H12m-9.75 1.51c0 1.6 1.123 2.994 2.707 3.227 1.129.166 2.27.293 3.423.379.35.026.67.21.865.501L12 21l2.755-4.133a1.14 1.14 0 0 1 .865-.501 48.172 48.172 0 0 0 3.423-.379c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0 0 12 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018Z"
+                    />
+                  </svg>
+                  Comment
+                </button>
               </div>
 
               <div className="comment-section">
@@ -149,7 +174,7 @@ function App() {
               </div>
 
               <ul className="comment-list">
-                {Object.values(post.comments).map((comment, j) => (
+                {post.comments.map((comment, j) => (
                   <li key={j}>
                     <div className="comment-user">
                       <img
--- social-media/src/data.js
+++ social-media/src/data.js
@@ -1,44 +1,47 @@
 export const userAccount = {
-  owner: "Dario Jularic",
-  place: "Zagreb, Croatia",
+  owner: "Lazar Vuckovic",
+  place: "Smederevo, Serbia",
   picture: "avatar-image2.jpg",
+  friends: [
+    { name: "Emily Johnson", picture: "profile1.jpg" },
+    { name: "Jason Smith", picture: "profile2.jpg" },
+    { name: "Stephan Williams", picture: "profile3.jpg" },
+    { name: "James Brown", picture: "profile4.jpg" },
+    { name: "Oliveira Jones", picture: "profile5.jpg" },
+    { name: "Daniel Miller", picture: "profile6.jpg" },
+  ],
+  posts: [
+    {
+      // date-fns
+      // how to get six months ago
+      // react free library icons
+      date: "about 6 months ago",
+      text: "This platform is all about thoughtful reflections.",
+      likes: [{ name: "James Brown" }, { name: "Oliveira Jones" }],
+      comments: [
+        {
+          profile: "James Brown",
+          text: "Makes me ponder on the importance of staying true to one's moral compass.",
+          image: "profile4.jpg",
+        },
+        {
+          profile: "Oliveira Jones",
+          text: "Sometimes the best option is to stick to your principles.",
+          image: "profile5.jpg",
+        },
+      ],
+    },
+    {
+      date: "about 1 year ago",
+      text: "The world doesn't need more noise. It needs more meaning.",
+      likes: [{ name: "Daniel Miller" }],
+      comments: [
+        {
+          profile: "Daniel Miller",
+          text: "Couldn't agree more.",
+          image: "profile6.jpg",
+        },
+      ],
+    },
+  ],
 };
-
-export const friendsData = [
-  { name: "Emily Johnson", picture: "profile1.jpg" },
-  { name: "Jason Smith", picture: "profile2.jpg" },
-  { name: "Stephan Williams", picture: "profile3.jpg" },
-  { name: "James Brown", picture: "profile4.jpg" },
-  { name: "Oliveira Jones", picture: "profile5.jpg" },
-  { name: "Daniel Miller", picture: "profile6.jpg" },
-];
-
-export const fixedPosts = [
-  {
-    text: "This platform is all about thoughtful reflections.",
-    likes: [{ name: "James Brown" }, { name: "Oliveira Jones" }],
-    comments: [
-      {
-        profile: "James Brown",
-        text: "Makes me ponder on the importance of staying true to one's moral compass.",
-        image: "profile4.jpg",
-      },
-      {
-        profile: "Oliveira Jones",
-        text: "Sometimes the best option is to stick to your principles.",
-        image: "profile5.jpg",
-      },
-    ],
-  },
-  {
-    text: "The world doesn't need more noise. It needs more meaning.",
-    likes: [{ name: "Daniel Miller" }],
-    comments: [
-      {
-        profile: "Daniel Miller",
-        text: "Couldn't agree more.",
-        image: "profile6.jpg",
-      },
-    ],
-  },
-];
